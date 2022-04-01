9c6e73351ae82bd9f8d9e3fca1fc76c4470155f6
Andrei Chigileichik <andrei.chigileichik@intexsoft.by>
2022-04-01 20:18:02 +0300

    Add pipe validation file and create class ValidationPipe. Use global pipe un main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,25 @@
-import {NestFactory} from "@nestjs/core";
-import {AppModule} from "./app.module";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from './pipes/validation.pipe';
 
 async function start() {
-    const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
 
-    const config = new DocumentBuilder()
-        .setTitle('Computer-shop server')
-        .setDescription('Documentation REST API')
-        .setVersion('1.0.0')
-        .addTag('Computer SHOP')
-        .build()
+  const config = new DocumentBuilder()
+    .setTitle('Computer-shop server')
+    .setDescription('Documentation REST API')
+    .setVersion('1.0.0')
+    .addTag('Computer SHOP')
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('/api/docs', app, document)
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
-    await app.listen(PORT, () => console.log(`SERVER RUN ON PORT = ${PORT}`));
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT, () => console.log(`SERVER RUN ON PORT = ${PORT}`));
 }
 
-start()
+start();
