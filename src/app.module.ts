d0b16567232df6c9c853a256931c7c7fc73b5eff
Andrei Chigileichik <andrei.chigileichik@intexsoft.by>
2022-04-01 20:13:18 +0300

    add main and app.module files

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import {Module} from "@nestjs/common";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+
+
+@Module({
+    controllers: [],
+    providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [],
+            autoLoadModels: true,
+        }),
+    ],
+})
+export class AppModule {
+
+}
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
+
+async function start() {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Computer-shop server')
+        .setDescription('Documentation REST API')
+        .setVersion('1.0.0')
+        .addTag('Computer SHOP')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/api/docs', app, document)
+
+
+    await app.listen(PORT, () => console.log(`SERVER RUN ON PORT = ${PORT}`));
+}
+
+start()
